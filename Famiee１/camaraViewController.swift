f76aa8437a310dead32e97cac0b5dc68679f88d3
Takagishi Kenshin <h.r.y.kt629@gmail.com>
2019-03-15 17:45:48 +0900

    local change

--- Famiee１/camaraViewController.swift
+++ Famiee１/camaraViewController.swift
@@ -15,7 +15,7 @@ class camaraViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Do any additional setup loading the view.
     }
 
     /*
