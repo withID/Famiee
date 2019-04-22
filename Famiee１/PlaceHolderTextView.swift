eefcb7c90c33dcad1117c12a13a4570ac9b9a3d0
taiki <nikora0425@icloud.com>
2019-04-22 16:33:00 +0900

    一旦

---
+++ Famiee１/PlaceHolderTextView.swift
@@ -0,0 +1,21 @@
+//
+//  PlaceHolderTextView.swift
+//  Famiee１
+//
+//  Created by taiki on 2019/04/20.
+//  Copyright © 2019 高岸　憲伸. All rights reserved.
+//
+
+import UIKit
+
+class PlaceHolderTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
